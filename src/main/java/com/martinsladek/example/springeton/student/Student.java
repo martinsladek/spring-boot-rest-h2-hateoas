adc88d51ef2ced193e015626f41ea1b84383b76f
Martin Sladek <martinsladek@users.noreply.github.com>
2022-10-04 01:57:07 +0200

    FIX: auto id generator ignores entities created by data.sql Student:  component: entity;  updated: id generator type switched from auto to sequence generator with appropriate initial value

--- src/main/java/com/martinsladek/example/springeton/student/Student.java
+++ src/main/java/com/martinsladek/example/springeton/student/Student.java
@@ -14,6 +14,8 @@ import javax.persistence.Table;
 import com.martinsladek.example.springeton.lesson.Lesson;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 @Entity
 @Table(name = "student")
@@ -21,13 +23,27 @@ public class Student {
     @Id
     // Generation Types: AUTO, IDENTITY, SEQUENCE and TABLE
     // https://www.baeldung.com/hibernate-identifiers#generated-identifiers
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "student_sequence"),
+                    @Parameter(name = "initial_value", value = "100"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_generator")
+//    @SequenceGenerator(name="student_generator", sequenceName = "student_seq", allocationSize=50)
     private Long id;
     private String name;
     private boolean active;
     private int credits;
 
     @ManyToMany
+//    Optionally define cascade:
+//    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
     @JoinTable(
             name = "student_lesson",
             joinColumns = @JoinColumn(name = "student_id"),
@@ -39,6 +55,12 @@ public class Student {
     private Set<Lesson> lessons;
 
     public Student() {
+    }
+
+    public Student(String name, boolean active, int credits) {
+        this.name = name;
+        this.active = active;
+        this.credits = credits;
     }
 
     public Long getId() {
