87ea2d2a2cd99d42021c6cd7f068b549b150b4bf
Martin Sladek <martinsladek@users.noreply.github.com>
2022-10-04 03:54:34 +0200

    Lesson, Student:   lazy load of related entity

--- src/main/java/com/martinsladek/example/springeton/student/Student.java
+++ src/main/java/com/martinsladek/example/springeton/student/Student.java
@@ -2,14 +2,7 @@ package com.martinsladek.example.springeton.student;
 
 import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import com.martinsladek.example.springeton.lesson.Lesson;
 
@@ -46,7 +39,7 @@ public class Student {
     private boolean active;
     private int credits;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
 //    Optionally define cascade:
 //    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
     @JoinTable(
