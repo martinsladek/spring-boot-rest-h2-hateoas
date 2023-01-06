2ba8ece27912d33a25b9ea0d5cd2fd6231e0d86a
Martin Sladek <martinsladek@users.noreply.github.com>
2023-01-06 16:59:18 +0100

    SQL init files - moved to test resources and to project templates schema.sql - init sql file not required; init done by hibernate ddl-auto data.sql - init data moved to test resources; not required in main dir; could even break liquibase

---
+++ src/test/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO `student` (name, active, credits) VALUES ('Adele', 1, 30);
+INSERT INTO `student` (name, active, credits) VALUES ('Freddy', 1, 40);
+INSERT INTO `student` (name, active, credits) VALUES ('John', 1, 50);
+INSERT INTO `student` (name, active, credits) VALUES ('Michael', 1, 60);
+INSERT INTO `student` (name, active, credits) VALUES ('Steve', 1, 70);
+
+
+INSERT INTO `lesson` (name) VALUES ('singing');
+INSERT INTO `lesson` (name) VALUES ('playing');
+INSERT INTO `lesson` (name) VALUES ('performing');
+
+
+INSERT INTO `student_lesson` (student_id, lesson_id) VALUES (1, 1);
+INSERT INTO `student_lesson` (student_id, lesson_id) VALUES (2, 2);
